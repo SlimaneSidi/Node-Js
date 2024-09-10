961a1f8cd87debe9b0fac941ff2a3ee2116a5f1f
Robin <rnullans06@gmail.com>
2024-09-10 11:09:35 +0200

    html2

--- TD0/index.js
+++ TD0/index.js
@@ -1,1 +1,9 @@
-console.log('Hello, world!');
+// Express app entry point : Basic server setup localhost:8080
+
+// import express module
+
+const express = require('express');
+ 
+// start server on port 8080
+
+const app = express();
