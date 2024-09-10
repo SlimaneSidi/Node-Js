7ee7f99171ce8df668cc7e15219a1970c42cb8b6
Robin <rnullans06@gmail.com>
2024-09-10 11:26:02 +0200

    html3+localhost

--- TD0/index.js
+++ TD0/index.js
@@ -7,3 +7,15 @@ const express = require('express');
 // start server on port 8080
 
 const app = express();
+
+// create a route for the home page index.html
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+// start server on port 8080
+
+app.listen(8080, () => {
+    console.log('Server started on http://localhost:8080');
+});
