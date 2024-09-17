26bca3b52b0468d42aa8f90cb77fa1647cb59392
Robin <rnullans06@gmail.com>
2024-09-17 11:12:03 +0200

    4TD1

--- TD1/server.js
+++ TD1/server.js
@@ -1,24 +1,38 @@
-const fs = require('fs').promises;
-
 const http = require('http');
+
+const axios = require('axios');
 
 const server = http.createServer(async (req, res) => {
 
+if (req.url === '/spells') {
+
 try {
 
-const data = await fs.readFile('./message.txt', 'utf8');
+const response = await axios.get('https://www.dnd5eapi.co/api/spells');
+
+const spells = response.data.results;
 
 res.statusCode = 200;
 
-res.setHeader('Content-Type', 'text/plain');
+res.setHeader('Content-Type', 'application/json');
 
-res.end(data);
+res.end(JSON.stringify(spells));
 
 } catch (err) {
 
+console.error(err);
+
 res.statusCode = 500;
 
-res.end('Erreur interne du serveur.');
+res.end("Erreur lors de la récupération des sorts.");
+
+}
+
+} else {
+
+res.statusCode = 404;
+
+res.end("Route non trouvée.");
 
 }
 
